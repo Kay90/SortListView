4476c54bb915049446acbc100d55988e3314443d
Kaizen <wangkai.kaizen@gmail.com>
2015-08-22 15:44:47 +0800

    initial commit

---
+++ app/src/main/java/cn/k/sortlistview/SortModel.java
@@ -0,0 +1,27 @@
+package cn.k.sortlistview;
+
+/**
+ * Created by Kaizen on 2015/8/22.
+ */
+public class SortModel {
+
+    private String name;
+
+    private String sortLetters;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSortLetters() {
+        return sortLetters;
+    }
+
+    public void setSortLetters(String sortLetters) {
+        this.sortLetters = sortLetters;
+    }
+}
